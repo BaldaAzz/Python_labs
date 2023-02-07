aa3a67df3ad77ab4aecbd221abc9bcb4493b1a2c
Daniil <124627774+SISDRUG@users.noreply.github.com>
2023-02-07 17:18:06 +0300

    Delete laba2.3.py

--- laba2.3.py
+++
@@ -1,9 +0,0 @@
-num1=int(input('введите число '))
-num2=int(input('введите число '))
-if num1>num2 :
-    print('разница ', num1-num2)
-elif num1<num2 :
-    print('сумма ', num1+num2)
-else:
-    print('равны ', num1)
-    
