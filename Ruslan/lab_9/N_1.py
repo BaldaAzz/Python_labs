e3b9aa73022d522e771ffb79518a673a6de30eb7
BaldaAzz <124623842+BaldaAzz@users.noreply.github.com>
2023-02-08 21:21:03 +0300

    Delete Ruslan/lab_9 directory

--- Ruslan/lab_9/N_1.py
+++
@@ -1,8 +0,0 @@
-lst = [1, 2, 3, 4, 5, 6, 7, 8]
-print(lst)
-
-def rearrange_list(list_nums, step):
-    step %= len(list_nums)
-    return list_nums[step::] + lst[:step:]
-
-print(rearrange_list(lst, int(input())))
