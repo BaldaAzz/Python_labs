a1397f81efb91495fdd2bb829b9d96be767a96f4
BaldaAzz <kill200419@gmail.com>
2023-02-20 13:46:47 +0300

    Положил болт на 19 лабу

--- Ruslan/lab_19/N_2.py
+++ Ruslan/lab_19/N_2.py
@@ -3,60 +3,32 @@ from wand.color import Color
 from wand.drawing import Drawing
 from wand.display import display
 
-center_coords = (200, 200)
-            # Толщина: радиус
-circles = {2: (80, 80),
-           3: (100, 100)}
-            # Толщина: (Начало, конец)
-lines = {1: (((30, 200), (370, 200)), 
-             ((200, 30), (200, 370))),
-         3: (((30, 200), (130, 200)),
-             ((200, 30), (200, 370)),
-             ((200, 30), (200, 130)),
-             ((200, 270), (200, 370)),
-             ((180, 200), (190, 200)),
-             ((210, 200), (220, 200)),
-             ((200, 180), (200, 190)),
-             ((200, 210), (200, 220)))}
 
 img = Wandlmage(height=400, width=400, background=Color('white'))
 draw = Drawing()
 draw.fill_color = Color('white')
 draw.stroke_color = Color('black')
 
-for sk_width, radius in circles.items():
-    print(radius, sk_width)
-    draw.stroke_width = sk_width
-    draw.circle(center_coords, radius)
-    draw.draw(img)
-    draw.clear
 
-for sk_width, coords in lines.items():
-    draw.stroke_width = sk_width
-    for i in coords:
-        draw.line(i[0], i[1])
-        draw.draw(img)
-        draw.clear
+draw.stroke_width = 3
+draw.ellipse((200, 200), (100, 100))
+draw.stroke_width = 2
+draw.ellipse((200, 200), (80, 80))
 
-# draw.stroke_width = 3
-# draw.ellipse((200, 200), (100, 100))
-# draw.stroke_width = 2
-# draw.ellipse((200, 200), (80, 80))
+draw.stroke_width = 1
+draw.line((30, 200), (370, 200))
+draw.line((200, 30), (200, 370))
+ 
+draw.stroke_width = 3
+draw.line((30, 200), (130, 200))
+draw.line((270, 200), (370, 200))
 
-# draw.stroke_width = 1
-# draw.line((30, 200), (370, 200))
-# draw.line((200, 30), (200, 370))
+draw.line((200, 30), (200, 130))
+draw.line((200, 270), (200, 370))
 
-# draw.stroke_width = 3
-# draw.line((30, 200), (130, 200))
-# draw.line((270, 200), (370, 200))
-
-# draw.line((200, 30), (200, 130))
-# draw.line((200, 270), (200, 370))
-
-# draw.line((180, 200), (190, 200))
-# draw.line((210, 200), (220, 200))
-# draw.line((200, 180), (200, 190))
-# draw.line((200, 210), (200, 220))
+draw.line((180, 200), (190, 200))
+draw.line((210, 200), (220, 200))
+draw.line((200, 180), (200, 190))
+draw.line((200, 210), (200, 220))
 draw.draw(img)
-img.save(filename ='tt.bmp')
+display(img)
