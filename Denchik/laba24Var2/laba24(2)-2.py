18377f0e5d4be4fdcd18e637ab6feacb5b87abcc
Daniil <124627774+SISDRUG@users.noreply.github.com>
2023-03-01 14:20:57 +0300

    Update laba24(2)-2.py

--- Denchik/laba24Var2/laba24(2)-2.py
+++ Denchik/laba24Var2/laba24(2)-2.py
@@ -1,6 +1,5 @@
-# Задана матрица 4х8. Возвести элементы матрицы в куб.
+"Задана матрица 4х8. Возвести элементы матрицы в куб."
 
-from random import randint
 import numpy as np
 
 mass = np.array(np.random.randint(-10, 10, 32)).reshape(4, 8)
