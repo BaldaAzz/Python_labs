8d00aafcf72f11c10256740e98084523f53f2d86
BaldaAzz <kill200419@gmail.com>
2023-03-01 11:26:26 +0300

    laba25Var2 Complete

---
+++ Denchik/laba25Var2/laba25(2)-3.py
@@ -0,0 +1,12 @@
+# 3.	В одномерном массиве найти произведение элементов,
+# находящихся между минимальным и максимальным элементами.
+# Сами минимальный и максимальный элементы в произведение не включать.
+
+import numpy as np
+
+mass = np.array(np.random.randint(0, 10, 10), dtype=int)
+print(mass)
+if np.argmin(mass) < np.argmax(mass):
+    print(np.prod(mass[np.argmin(mass) + 1  : np.argmax(mass)])) 
+else:
+    print(np.prod(mass[np.argmax(mass) + 1  : np.argmin(mass)]))
