88b454e12bd1a5c8332396d05a97c620d8221703
BaldaAzz <124623842+BaldaAzz@users.noreply.github.com>
2023-03-01 09:48:34 +0300

    Update N_3

--- Ruslan/lab_24/N_3.py
+++ Ruslan/lab_24/N_3.py
@@ -4,8 +4,8 @@ import numpy as np
 matrix = np.array(np.arange(0, 9), dtype=int).reshape(3,3)
 print(matrix)
 
-for i in range(len(matrix)):
-    for j in range(len(matrix[i])):
+for i in range(matrix.shape[0]):
+    for j in range(matrix.shape[1]):
         if i == j:
             matrix[i, j] = 1
 
