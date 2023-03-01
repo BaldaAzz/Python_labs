52ca4f405a14d5f5cd1969f0fc30a0995d1085ca
Daniil <124627774+SISDRUG@users.noreply.github.com>
2023-03-01 14:21:14 +0300

    Update laba24(2)-3.py

--- Denchik/laba24Var2/laba24(2)-3.py
+++ Denchik/laba24Var2/laba24(2)-3.py
@@ -1,17 +1,14 @@
-# Задан одномерный массив размерностью 10. 
-# Переформатировать в двумерныймассив.
-# Элементы главной диагонали заменить на 0.
+"""Задан одномерный массив размерностью 10. 
+Переформатировать в двумерныймассив.
+Элементы главной диагонали заменить на 0."""
 
 import numpy as np
 
-mass = np.arange(10)
+mass = np.arange(10, dtype=int)
 mass = mass.reshape(2,5)
 
 print(mass)
 
-for i in range(len(mass)):
-    for j in range(len(mass[0])):
-        if i == j:
-            mass[i][j] = 0
+np.fill_diagonal(mass, 0)
 
 print(mass)
