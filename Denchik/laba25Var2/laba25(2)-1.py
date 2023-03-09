11c9b2c98b278bf821eb42bca5a20349b895609b
SISDRUG <drugdanik@gmail.com>
2023-03-09 14:08:52 +0300

    Laba25Var2 Fixies

--- Denchik/laba25Var2/laba25(2)-1.py
+++ Denchik/laba25Var2/laba25(2)-1.py
@@ -1,15 +1,16 @@
-"""1. Даны два двумерных массива.
+"""
+1. Даны два двумерных массива.
  Посчитайте сумму транспонированных матриц,произведение матриц.
  Определите, какая сумма больше."""
 
 import numpy as np
 
-mass_1 = np.array(np.random.randint(-10, 10, 25)).reshape(5, 5)
-mass_2 = np.array(np.random.randint(-10, 10, 25)).reshape(5, 5)
+mass_1 = np.random.randint(-10, 10, (5, 5))
+mass_2 = np.random.randint(-10, 10, (5, 5))
 
 mass_sum = (mass_1 + mass_2).T #Транспориванная матрица сумм первой и второй матрицы
 mass_multi = mass_1 * mass_2   #Матрица произведния первой и второй матрицы 
-mass_sum_isbiggest = 'больше сумма первого' if np.sum(mass_1) > np.sum(mass_2) else 'больше сумма второго'
+mass_sum_isbiggest = 'Больше сумма первого' if np.sum(mass_1) > np.sum(mass_2) else 'Больше сумма второго'
 
 
 
--- Denchik/laba25Var2/laba25(2)-2.py
+++ Denchik/laba25Var2/laba25(2)-2.py
@@ -1,14 +1,20 @@
 """2.	Дан двумерный массив размерностью 5х6.
 Определите индекс максимального и минимального элемента, на место максимального поставьте минимальный и наоборот."""
 
-import numpy as np 
+import numpy as np
 
-mass = np.arange(5*6).reshape(5,6)
+mass = np.random.randint(0, 10, (5, 6))
 
 print(mass)
-mass_max = np.argmax(mass)
-mass_min = np.argmin(mass)
 
-print('index max element(', mass_max%mass.shape[0],',',mass_max//mass.shape[0],')')
-print('index min element(', mass_min%mass.shape[0],',',mass_min//mass.shape[0],')')
+mass_max = np.argmax(mass.reshape(1, 30))
+mass_min = np.argmin(mass.reshape(1, 30))
+mass_max_index = [mass_max // mass.shape[0],mass_max % mass.shape[1]]
+mass_min_index = [mass_min // mass.shape[0],mass_min % mass.shape[1]]
+
+print(f'index max: {mass_max_index}')
+print(f'index min: {mass_min_index}')
+
+mass[mass_max_index[0]][mass_max_index[1]], mass[mass_min_index[0]][mass_min_index[1]] = mass[mass_min_index[0]][mass_min_index[1]], mass[mass_max_index[0]][mass_max_index[1]]
+
 print(mass)
