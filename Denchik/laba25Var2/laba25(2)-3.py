f6427ed5ffa9a44e7ea529e6f97521d3e7c5d4a8
SISDRUG <drugdanik@gmail.com>
2023-03-14 18:39:24 +0300

    Laba25-26Var2 Comit new morj

--- Denchik/laba25Var2/laba25(2)-3.py
+++ Denchik/laba25Var2/laba25(2)-3.py
@@ -11,6 +11,13 @@ mass = np.random.randint(0, 10, 10)
 print(mass)
 
 if np.argmin(mass) < np.argmax(mass):
+    print(np.prod(mass[np.argmin(mass) + 1: np.argmax(mass)]))
+else:
+    print(np.prod(mass[np.argmax(mass) + 1: np.argmin(mass)]))
+    
+""" := морж
+if np.argmin(mass) < np.argmax(mass):
     print(minmax_prod := np.prod(mass[np.argmin(mass) + 1: np.argmax(mass)]))
 else:
-    print(minmax_prod := np.prod(mass[np.argmax(mass) + 1: np.argmin(mass)]))
+    print(minmax_prod := np.prod(mass[np.argmax(mass) + 1: np.argmin(mass)])) 
+    """
