bd35aa93e1bb39963d7a19186fe9264718f9ee65
dima shvedko <dima.shvedko.90@mail.ru>
2023-03-29 22:38:34 +0300

    da

--- Dmitriy/labs 23/23_3_4.py
+++ Dmitriy/labs 23/23_3_4.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-array = np.array(np.arange(10).reshape(2,5),float)
+array = np.array(np.arange(10).reshape(2, 5))
 array.fill(5)
 
 print(array)
---
+++ Dmitriy/labs 29/labs 29_1.py
@@ -0,0 +1,4 @@
+import numpy as np
+import pandas as pd
+
+array = np.array(np.arange(10))
